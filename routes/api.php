b20fae785dbf06fa0adba7ca3b45d9a01a58ce34
haileybronson <haileybronsonphotos@gmail.com>
2025-03-13 18:11:26 -0600

    finished !

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\UserController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
